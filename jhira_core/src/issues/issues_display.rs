fa011ab649a4926092b1ff1c50f6c96396a94b94
Shane Osbourne <shane.osbourne8@gmail.com>
2020-03-03 22:33:25 +0000

    example of converting data to display

--- jhira_core/src/issues/issues_display.rs
+++ jhira_core/src/issues/issues_display.rs
@@ -19,7 +19,16 @@ impl AsyncTask for IssuesDisplay {
     async fn exec(&self) -> Result<TaskOutput, failure::Error> {
         let resp = self.resp.lock().unwrap();
         let resp_string = resp.clone().ok_or(IssuesDisplayError::Missing)?;
-        let _parsed_issues = JiraIssues::from_str(&resp_string)?;
-        Ok(TaskOutput::Done)
+        let output = display(JiraIssues::from_str(&resp_string)?);
+        Ok(TaskOutput::String(vec![output]))
     }
+}
+
+fn display(issues: JiraIssues) -> String {
+    return issues
+        .issues
+        .into_iter()
+        .map(|issue| issue.key)
+        .collect::<Vec<String>>()
+        .join(",");
 }
